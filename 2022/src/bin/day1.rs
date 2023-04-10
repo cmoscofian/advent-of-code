08a6f82596065c385635c7f19f4201cc20dc4330
Charles Moscofian <charlesmoscofian@hotmail.com>
2023-04-10 23:45:39 +0200

    feat(2022): [day-1] solve second part

--- 2022/src/bin/day1.rs
+++ 2022/src/bin/day1.rs
@@ -2,8 +2,12 @@ use aoc::get_input;
 
 fn main() {
     let input = get_input("01");
+
     let day1_first = first(&input);
     println!("day1-first: {:?}", day1_first);
+
+    let day1_second = second(input);
+    println!("day1-second: {:?}", day1_second);
 }
 
 fn first(input: &String) -> u32 {
@@ -18,6 +22,20 @@ fn first(input: &String) -> u32 {
         .expect("should return the max value")
 }
 
+fn second(input: String) -> u32 {
+    let mut resp = input
+        .split("\n\n")
+        .map(|elve| {
+            elve.lines()
+                .map(|x| x.parse::<u32>().expect("should parse string"))
+                .sum::<u32>()
+        })
+        .collect::<Vec<u32>>();
+
+    resp.sort_by(|x, y| y.cmp(x));
+    resp.iter().take(3).sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -28,5 +46,12 @@ mod tests {
         let input = read_to_string("input/01/example").expect("should read file successfully");
         let response = first(&input);
         assert_eq!(24000, response);
+    }
+
+    #[test]
+    fn test_second() {
+        let input = read_to_string("input/01/example").expect("should read file successfully");
+        let response = second(input);
+        assert_eq!(45000, response);
     }
 }
