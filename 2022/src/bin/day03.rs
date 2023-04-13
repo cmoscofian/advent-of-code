1b994ddd6314198237229347257d2beec2b3def0
Charles Moscofian <charlesmoscofian@hotmail.com>
2023-04-13 23:59:25 +0200

    feat(2022): [day-3] solve second part

--- 2022/src/bin/day03.rs
+++ 2022/src/bin/day03.rs
@@ -7,6 +7,9 @@ fn main() {
 
     let day3_first = first(&input);
     println!("first: {day3_first}");
+
+    let day3_second = second(input);
+    println!("second: {day3_second}");
 }
 
 fn first(input: &String) -> u32 {
@@ -14,22 +17,61 @@ fn first(input: &String) -> u32 {
         .lines()
         .map(|r| {
             let (first, second) = r.split_at(r.len() / 2);
-            let cmp_1 = std::collections::HashSet::<char>::from_iter(first.chars());
-            let cmp_2 = std::collections::HashSet::<char>::from_iter(second.chars());
-            let inter = cmp_1.intersection(&cmp_2).collect::<HashSet<&char>>();
-            let repeated_item = inter
-                .iter()
+            let cmp_1 = HashSet::<char>::from_iter(first.chars());
+            let cmp_2 = HashSet::<char>::from_iter(second.chars());
+            let duplicated_item = cmp_1
+                .intersection(&cmp_2)
                 .next()
                 .expect("should return a single value from intersection");
-            calculate_value(repeated_item)
+            calculate_value(duplicated_item)
         })
         .sum::<u32>()
 }
 
-fn calculate_value(c: &&char) -> u32 {
-    match (**c).is_uppercase() {
-        true => (**c) as u32 - 38,
-        false => (**c) as u32 - 96,
+fn second(input: String) -> u32 {
+    let mut result = 0;
+
+    let mut idx = 0;
+    let lines_vec = input.lines().collect::<Vec<&str>>();
+    for _ in 0..input.lines().count() / 3 {
+        let cmp_1 = HashSet::<char>::from_iter(
+            lines_vec
+                .get(idx)
+                .expect("should return a valid line")
+                .chars(),
+        );
+        let cmp_2 = HashSet::<char>::from_iter(
+            lines_vec
+                .get(idx + 1)
+                .expect("should return a valid line")
+                .chars(),
+        );
+        let cmp_3 = HashSet::<char>::from_iter(
+            lines_vec
+                .get(idx + 2)
+                .expect("should return a valid line")
+                .chars(),
+        );
+
+        idx += 3;
+
+        let badge = cmp_1
+            .iter()
+            .filter(|k| cmp_2.contains(k))
+            .filter(|k| cmp_3.contains(k))
+            .next()
+            .expect("should return a single repeated value from the rucksacks");
+
+        result += calculate_value(badge);
+    }
+
+    result
+}
+
+fn calculate_value(c: &char) -> u32 {
+    match (*c).is_uppercase() {
+        true => (*c) as u32 - 38,
+        false => (*c) as u32 - 96,
     }
 }
 
@@ -43,5 +85,12 @@ mod tests {
         let input = read_to_string("input/03/example").expect("should read file successfully");
         let response = first(&input);
         assert_eq!(157, response);
+    }
+
+    #[test]
+    fn test_second() {
+        let input = read_to_string("input/03/example").expect("should read file successfully");
+        let response = second(input);
+        assert_eq!(70, response);
     }
 }
