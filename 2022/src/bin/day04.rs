3a05fdf93597f989b45681c639d90ffb17a98ef8
Charles Moscofian <charlesmoscofian@hotmail.com>
2023-04-15 00:34:46 +0200

    feat(2022): [day-4] solve first part

---
+++ 2022/src/bin/day04.rs
@@ -0,0 +1,61 @@
+use aoc::get_input;
+
+struct Section {
+    first: (u32, u32),
+    second: (u32, u32),
+}
+
+impl Section {
+    fn check_contains(self) -> bool {
+        self.first.0 >= self.second.0 && self.first.1 <= self.second.1
+            || self.first.0 <= self.second.0 && self.first.1 >= self.second.1
+    }
+}
+
+impl std::str::FromStr for Section {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let values = s
+            .split(',')
+            .flat_map(|f| f.split('-').map(|x| x.parse::<u32>().unwrap()))
+            .collect::<Vec<_>>();
+
+        let a = values.get(0).unwrap().clone();
+        let b = values.get(1).unwrap().clone();
+        let c = values.get(2).unwrap().clone();
+        let d = values.get(3).unwrap().clone();
+
+        Ok(Self {
+            first: if a < b { (a, b) } else { (b, a) },
+            second: if c < d { (c, d) } else { (d, c) },
+        })
+    }
+}
+
+fn main() {
+    let input = get_input("04");
+
+    let day4_first = first(&input);
+    println!("first: {day4_first}");
+}
+
+fn first(input: &String) -> u32 {
+    input
+        .lines()
+        .filter(|x| x.parse::<Section>().unwrap().check_contains())
+        .count() as u32
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::read_to_string;
+
+    #[test]
+    fn test_first() {
+        let input = read_to_string("input/04/example").expect("should read file successfully");
+        let response = first(&input);
+        assert_eq!(2, response);
+    }
+}
