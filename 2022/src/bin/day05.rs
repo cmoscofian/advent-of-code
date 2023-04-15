818ccbef6c3f266fceb5ade3b560186a31ee26c5
Charles Moscofian <charlesmoscofian@hotmail.com>
2023-04-15 20:56:58 +0200

    feat(2022): [day-5] solve second part

--- 2022/src/bin/day05.rs
+++ 2022/src/bin/day05.rs
@@ -5,27 +5,32 @@ fn main() {
 
     let day5_first = first(&input);
     println!("first: {day5_first}");
+
+    let day5_second = second(input);
+    println!("second: {day5_second}");
 }
 
 struct Ship {
     keys: Vec<usize>,
-    data: std::collections::HashMap<usize, Vec<char>>,
+    data: std::collections::HashMap<usize, std::collections::VecDeque<char>>,
 }
 
 impl std::str::FromStr for Ship {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut data = std::collections::HashMap::<usize, Vec<char>>::new();
+        let mut data = std::collections::HashMap::<usize, std::collections::VecDeque<char>>::new();
         let mut keys = Vec::new();
         s.lines().rev().skip(1).for_each(|f| {
-            f.char_indices().for_each(|(idx, f)| match f {
+            f.char_indices().for_each(|(i, c)| match c {
                 'A'..='Z' | 'a'..='z' => {
-                    if let Some(value) = data.get_mut(&idx) {
-                        value.push(f);
+                    if let Some(value) = data.get_mut(&i) {
+                        value.push_back(c);
                     } else {
-                        data.insert(idx, vec![f]);
-                        keys.push(idx);
+                        let mut deque = std::collections::VecDeque::new();
+                        deque.push_back(c);
+                        data.insert(i, deque);
+                        keys.push(i);
                     };
                 }
                 _ => {}
@@ -39,16 +44,30 @@ impl std::str::FromStr for Ship {
 }
 
 impl Ship {
-    fn execute(&mut self, instruction: Instruction) {
+    fn createmover9000(&mut self, instruction: Instruction) {
         for _ in 0..instruction.moves {
             let from_key = self.keys.get(instruction.from - 1).unwrap();
             let to_key = self.keys.get(instruction.to - 1).unwrap();
 
             let from_stack = self.data.get_mut(from_key).unwrap();
-            let value = from_stack.pop().unwrap();
+            let value = from_stack.pop_back().unwrap();
 
             let to_stack = self.data.get_mut(to_key).unwrap();
-            to_stack.push(value);
+            to_stack.push_back(value);
+        }
+    }
+
+    fn createmover9001(&mut self, instruction: Instruction) {
+        let from_key = self.keys.get(instruction.from - 1).unwrap();
+        let to_key = self.keys.get(instruction.to - 1).unwrap();
+
+        let from_stack = self.data.get_mut(from_key).unwrap();
+        let elements = from_stack
+            .drain(from_stack.len() - instruction.moves..)
+            .collect::<Vec<_>>();
+        let to_stack = self.data.get_mut(to_key).unwrap();
+        for element in elements.iter() {
+            to_stack.push_back(*element);
         }
     }
 
@@ -57,7 +76,7 @@ impl Ship {
 
         self.keys.iter().for_each(|f| {
             let vec = self.data.get(f).unwrap();
-            resp.push(*(vec.last().unwrap()));
+            resp.push(*(vec.back().unwrap()));
         });
 
         resp
@@ -93,7 +112,24 @@ fn first(input: &str) -> String {
         .collect::<Vec<Instruction>>();
 
     for instr in instructions {
-        ship.execute(instr);
+        ship.createmover9000(instr);
+    }
+
+    ship.get_top_of_stack()
+}
+
+fn second(input: String) -> String {
+    let data = input.split("\n\n").collect::<Vec<&str>>();
+    let mut ship = data.first().unwrap().parse::<Ship>().unwrap();
+    let instructions = data
+        .get(1)
+        .unwrap()
+        .lines()
+        .map(|l| l.parse::<Instruction>().unwrap())
+        .collect::<Vec<Instruction>>();
+
+    for instr in instructions {
+        ship.createmover9001(instr);
     }
 
     ship.get_top_of_stack()
@@ -109,5 +145,12 @@ mod tests {
         let input = read_to_string("input/05/example").expect("should read file successfully");
         let response = first(&input);
         assert_eq!("CMZ", response);
+    }
+
+    #[test]
+    fn test_second() {
+        let input = read_to_string("input/05/example").expect("should read file successfully");
+        let response = second(input);
+        assert_eq!("MCD", response);
     }
 }
