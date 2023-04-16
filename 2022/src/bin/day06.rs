6f5e1945d6cec2fc104b9427a8f32d4fe28c244e
Charles Moscofian <charlesmoscofian@hotmail.com>
2023-04-16 02:07:26 +0200

    feat(2022): [day-6] solve second part

--- 2022/src/bin/day06.rs
+++ 2022/src/bin/day06.rs
@@ -5,6 +5,9 @@ fn main() {
 
     let day6_first = first(&input);
     println!("first: {day6_first}");
+
+    let day6_second = second(input);
+    println!("second: {day6_second}");
 }
 
 fn first(input: &str) -> u32 {
@@ -22,6 +25,21 @@ fn first(input: &str) -> u32 {
     unreachable!();
 }
 
+fn second(input: String) -> u32 {
+    let chars = input.char_indices().collect::<Vec<(usize, char)>>();
+
+    for ch in chars.windows(14) {
+        let mut set = std::collections::HashSet::<char>::new();
+        let chars = ch.iter().map(|(_, c)| *c).collect::<Vec<_>>();
+        set.extend(chars);
+        if set.len() == 14 {
+            return ch.last().unwrap().0 as u32 + 1;
+        }
+    }
+
+    unreachable!();
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -32,5 +50,12 @@ mod tests {
         let input = read_to_string("input/06/example").expect("should read file successfully");
         let response = first(&input);
         assert_eq!(7, response);
+    }
+
+    #[test]
+    fn test_second() {
+        let input = read_to_string("input/06/example").expect("should read file successfully");
+        let response = second(input);
+        assert_eq!(19, response);
     }
 }
