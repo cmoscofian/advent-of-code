91052d8ca3c63a5730eabc8a2f4f8281570ff7db
Charles Moscofian <charlesmoscofian@hotmail.com>
2023-12-01 21:51:35 +0100

    feat(2023): [day-01] solve second part

--- 2023/src/bin/01.rs
+++ 2023/src/bin/01.rs
@@ -5,6 +5,9 @@ fn main() {
 
     let first = first(&input);
     println!("first: {first}");
+
+    let second = second(input);
+    println!("second: {second}");
 }
 
 fn first(input: &str) -> u32 {
@@ -24,6 +27,46 @@ fn first(input: &str) -> u32 {
         .sum::<u32>()
 }
 
+fn second(input: String) -> u32 {
+    let nums = [
+        "one", "1", "two", "2", "three", "3", "four", "4", "five", "5", "six", "6", "seven", "7",
+        "eight", "8", "nine", "9",
+    ];
+
+    let mut result = 0;
+
+    for line in input.lines() {
+        let mut first = None;
+        'first: for i in 0..line.len() {
+            for (index, num) in nums.iter().enumerate() {
+                if i + num.len() > line.len() {
+                    continue;
+                }
+                if line[i..i + num.len()] == **num {
+                    first = Some(1 + index / 2);
+                    break 'first;
+                }
+            }
+        }
+
+        let mut last = None;
+        'last: for i in (0..line.len()).rev() {
+            for (index, num) in nums.iter().enumerate() {
+                if i + num.len() > line.len() {
+                    continue;
+                }
+                if line[i..i + num.len()] == **num {
+                    last = Some(1 + index / 2);
+                    break 'last;
+                }
+            }
+        }
+
+        result += 10 * first.unwrap() as u32 + last.unwrap() as u32;
+    }
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -34,5 +77,12 @@ mod tests {
         let input = read_to_string("input/01/example").expect("should read file successfully");
         let response = first(&input);
         assert_eq!(142, response);
+    }
+
+    #[test]
+    fn test_second() {
+        let input = read_to_string("input/01/example2").expect("should read file successfully");
+        let response = second(input);
+        assert_eq!(281, response);
     }
 }
