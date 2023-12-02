e129ff35c671ae9d6541d877287e2225a11d4b0f
Charles Moscofian <charlesmoscofian@hotmail.com>
2023-12-02 16:25:05 +0100

    feat(2023): [day-02] solve second part

--- 2023/src/bin/02.rs
+++ 2023/src/bin/02.rs
@@ -8,16 +8,7 @@ struct Round {
 
 impl Round {
     fn is_valid(&self, other: &Round) -> bool {
-        if self.red > other.red {
-            return false;
-        }
-        if self.green > other.green {
-            return false;
-        }
-        if self.blue > other.blue {
-            return false;
-        }
-        true
+        self.red <= other.red && self.green <= other.green && self.blue <= other.blue
     }
 }
 
@@ -29,8 +20,8 @@ impl std::str::FromStr for Round {
         let mut green = 0;
         let mut blue = 0;
 
-        s.split(',')
-            .for_each(|l| match l.trim().split_once(' ').unwrap() {
+        s.split(", ")
+            .for_each(|l| match l.split_once(' ').unwrap() {
                 (n, "red") => red = n.parse::<u32>().unwrap(),
                 (n, "green") => green = n.parse::<u32>().unwrap(),
                 (n, "blue") => blue = n.parse::<u32>().unwrap(),
@@ -50,30 +41,37 @@ impl Game {
     fn is_valid(&self, boundary: &Round) -> bool {
         self.rounds.iter().filter(|r| !r.is_valid(boundary)).count() == 0
     }
+
+    fn min_required_cubes(&self) -> Round {
+        let mut red = 0;
+        let mut green = 0;
+        let mut blue = 0;
+
+        for round in &self.rounds {
+            red = red.max(round.red);
+            blue = blue.max(round.blue);
+            green = green.max(round.green);
+        }
+
+        Round { red, blue, green }
+    }
 }
 
 impl std::str::FromStr for Game {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (_first, _second) = s.split_once(':').unwrap();
-        let (_, _id) = _first.trim().split_once(' ').unwrap();
+        let (_first, _second) = s.split_once(": ").unwrap();
+        let (_, _id) = _first.split_once(' ').unwrap();
         let id = _id.parse::<u32>().unwrap();
 
         let rounds = _second
-            .split(';')
+            .split("; ")
             .map(|r| r.parse::<Round>().unwrap())
             .collect::<Vec<Round>>();
 
         Ok(Self { id, rounds })
     }
-}
-
-fn main() {
-    let input = get_input("02");
-
-    let first = first(&input);
-    println!("first: {first}");
 }
 
 fn first(input: &str) -> u32 {
@@ -90,6 +88,27 @@ fn first(input: &str) -> u32 {
         .sum()
 }
 
+fn second(input: String) -> u32 {
+    input
+        .lines()
+        .map(|l| {
+            let game = l.parse::<Game>().unwrap();
+            let cubes = game.min_required_cubes();
+            cubes.red * cubes.green * cubes.blue
+        })
+        .sum()
+}
+
+fn main() {
+    let input = get_input("02");
+
+    let first = first(&input);
+    println!("first: {first}");
+
+    let second = second(input);
+    println!("second: {second}");
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -100,5 +119,12 @@ mod tests {
         let input = read_to_string("input/02/example").expect("should read file successfully");
         let response = first(&input);
         assert_eq!(8, response);
+    }
+
+    #[test]
+    fn test_second() {
+        let input = read_to_string("input/02/example").expect("should read file successfully");
+        let response = second(input);
+        assert_eq!(2286, response);
     }
 }
