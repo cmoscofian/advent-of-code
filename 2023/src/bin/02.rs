b1d946de3e95c0e083b214c720b3698d40280d2d
Charles Moscofian <charlesmoscofian@hotmail.com>
2023-12-02 15:49:53 +0100

    feat(2023): [day-02] solve first part

---
+++ 2023/src/bin/02.rs
@@ -0,0 +1,104 @@
+use aoc::get_input;
+
+struct Round {
+    red: u32,
+    blue: u32,
+    green: u32,
+}
+
+impl Round {
+    fn is_valid(&self, other: &Round) -> bool {
+        if self.red > other.red {
+            return false;
+        }
+        if self.green > other.green {
+            return false;
+        }
+        if self.blue > other.blue {
+            return false;
+        }
+        true
+    }
+}
+
+impl std::str::FromStr for Round {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut red = 0;
+        let mut green = 0;
+        let mut blue = 0;
+
+        s.split(',')
+            .for_each(|l| match l.trim().split_once(' ').unwrap() {
+                (n, "red") => red = n.parse::<u32>().unwrap(),
+                (n, "green") => green = n.parse::<u32>().unwrap(),
+                (n, "blue") => blue = n.parse::<u32>().unwrap(),
+                (_, _) => unreachable!(),
+            });
+
+        Ok(Self { red, blue, green })
+    }
+}
+
+struct Game {
+    id: u32,
+    rounds: Vec<Round>,
+}
+
+impl Game {
+    fn is_valid(&self, boundary: &Round) -> bool {
+        self.rounds.iter().filter(|r| !r.is_valid(boundary)).count() == 0
+    }
+}
+
+impl std::str::FromStr for Game {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (_first, _second) = s.split_once(':').unwrap();
+        let (_, _id) = _first.trim().split_once(' ').unwrap();
+        let id = _id.parse::<u32>().unwrap();
+
+        let rounds = _second
+            .split(';')
+            .map(|r| r.parse::<Round>().unwrap())
+            .collect::<Vec<Round>>();
+
+        Ok(Self { id, rounds })
+    }
+}
+
+fn main() {
+    let input = get_input("02");
+
+    let first = first(&input);
+    println!("first: {first}");
+}
+
+fn first(input: &str) -> u32 {
+    let boundary = Round {
+        red: 12,
+        blue: 14,
+        green: 13,
+    };
+    input
+        .lines()
+        .map(|l| l.parse::<Game>().unwrap())
+        .filter(|g| g.is_valid(&boundary))
+        .map(|g| g.id)
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::read_to_string;
+
+    #[test]
+    fn test_first() {
+        let input = read_to_string("input/02/example").expect("should read file successfully");
+        let response = first(&input);
+        assert_eq!(8, response);
+    }
+}
